c7d9adb5b8902483369f0466f9ee41de48a91e1b
Darkstrike12 <108422951+Darkstrike12@users.noreply.github.com>
2023-12-06 12:58:43 -0600

    usar esc

---
+++ Assets/InputChecker.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputChecker : MonoBehaviour
+{
+    [SerializeField] GameScreen gameScreen;
+    [SerializeField] AudioController audioController;
+
+    public static InputChecker Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            switch (gameScreen)
+            {
+                case GameScreen.MainMenu:
+                    Application.Quit();
+                    break;
+                case GameScreen.OptionMenu:
+                    SceneManager.LoadScene("Menu_Pricipal");
+                    if(audioController != null)
+                    {
+                        audioController.OnMasterChange(1f);
+                        audioController.OnMusicToggle(true);
+                        audioController.OnSfxToggle(true);
+                    }
+                    break;
+                case GameScreen.Menu:
+                    SceneManager.LoadScene("Menu_Pricipal");
+                    break;
+                case GameScreen.Level:
+                    SceneManager.LoadScene("Menu_Niveles");
+                    break;
+            }
+        }
+    }
+
+    public enum GameScreen
+    {
+        none,
+        MainMenu,
+        OptionMenu,
+        Menu,
+        Level
+    }
+}
